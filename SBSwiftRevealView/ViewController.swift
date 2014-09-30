a759dc3054e908c0b73f155f4545e63098468680
Stephen Bero <stephen.m.bero@gmail.com>
2014-09-30 06:03:37 -0600

    Initial Commit

---
+++ SBSwiftRevealView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SBSwiftRevealView
+//
+//  Created by Stephen Bero on 9/30/14.
+//  Copyright (c) 2014 qws. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
